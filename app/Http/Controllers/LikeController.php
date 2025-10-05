9c72191f19558e635e234d4db69d277ead4daed6
minori Koshikawa <greenboy08031115@gmail.com>
2025-10-05 22:56:55 +0900

    商品詳細にいいねボタンを実装

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Item;
 
 class LikeController extends Controller
 {
     public function store($itemId)
     {
+        $user = Auth::user();
+        $item = Item::findOrFail($itemId);
+
+        $user->likes()->syncWithoutDetaching([$item->id]);
+
+        return back()->route('items.show');
 
     }
 
     public function destroy($itemId)
     {
+        $user = Auth::user();
+        $item = Item::findOrFail($itemId);
+
+        $user->likes()->detach($item->id);
+
+        return back()->route('items.show');
 
     }
 
--- resources/views/items/item.blade.php
+++ resources/views/items/item.blade.php
@@ -10,14 +10,16 @@
 <main class="item-show">
     <div class="item">
         <a class="item-card" href="#">
-            @if (filled($item->image_path))
-                <img class="item-card__image" src="{{ $item->image_path }}" alt="{{ $item->name }}">
-            @else
-                <div class="item-card__image item-card__image--placeholder">商品画像</div>
-            @endif
-            @if($item->is_sold)
-                <span class="item-card__sold">Sold</span>
-            @endif
+            <div class="item-card__thumb">
+                @if (filled($item->image_path))
+                    <img class="item-card__image" src="{{ $item->image_path }}" alt="{{ $item->name }}">
+                @else
+                    <div class="item-card__image item-card__image--placeholder">商品画像</div>
+                @endif
+                @if($item->is_sold)
+                    <span class="item-card__sold">Sold</span>
+                @endif
+            </div>
         </a>
 
         <div class="item-detail">
@@ -32,11 +34,32 @@
 
             <div class="item-show__meta">
                 <span class="item-show__likes">
-                    <svg class="icon icon--star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
-                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" fill="none" stroke="#333" stroke-width="2"/>
-                    </svg>
-                    {{ $item->likes_count }}
+                    @auth
+                        @if(!$item->likes->contains(auth()->user()))
+                            <form method="POST" action="{{ route('likes.store', $item->id) }}">
+                                @csrf
+                                <button class="like-button" type="submit">
+                                    {{ $item->likes_count }}
+                                    <svg class="icon icon--star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+                                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" fill="none" stroke="#333" stroke-width="2"/>
+                                    </svg>
+                                </button>
+                            </form>
+                        @else
+                            <form method="POST" action="{{ route('likes.destroy', $item->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button class="like-button like-button--active" type="submit">
+                                    {{ $item->likes_count }}
+                                    <svg class="icon icon--star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+                                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" fill="#ff5555" stroke="#ff5555" stroke-width="2"/>
+                                    </svg>
+                                </button>
+                            </form>
+                        @endif
+                    @endauth
                 </span>
+
                 <span class="item-show__comments">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M21 12c0 5-4 9-9 9a9 9 0 0 1-3-.5L5 21l1-3a9 9 0 1 1 15-6z"/>
@@ -51,7 +74,7 @@
                     <button {{ $item->is_sold ? 'disabled' : '' }}>購入手続きへ</button>
                 </form>
             @else
-                    <a class="button button--purchase" href="{{ route('login') }}">購入手続きへ</a>
+                <a class="button button--purchase" href="{{ route('login') }}">購入手続きへ</a>
             @endauth
 
             <h2>商品説明</h2>
@@ -61,13 +84,20 @@
 
             <h2>商品の情報</h2>
             @if ($item->categories->isNotEmpty())
-                <p>カテゴリー
-                    @foreach ($item->categories as $category)
-                        <span class="item-show__category-badge">{{ $category->name }}</span>
-                    @endforeach
-                </p>
+                <div class="item-show__category">
+                    <span class="item-show__category-label">カテゴリー</span>
+                    <div class="item-show__category-list">
+                        @foreach ($item->categories as $category)
+                            <span class="item-show__category-badge">{{ $category->name }}</span>
+                        @endforeach
+                    </div>
+                </div>
             @endif
-            <p>商品の状態 {{ $item->condition_label }}</p>
+
+            <p class="item-show__condition">
+                <span class="item-show__condition-label">商品の状態：</span>
+                {{ $item->condition_label }}
+            </p>
 
             <h2 class="comment-title">コメント ({{ $item->comments_count }})</h2>
             @if($comments->isNotEmpty())
